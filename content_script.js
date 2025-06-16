304169c62e06aa988c94a2a4e3e307635c55e378
SongYunSeop <dbstjq91@gmail.com>
2025-06-17 08:46:43 +0900

    www.cdec.kr 으로 변경

--- content_script.js
+++ content_script.js
@@ -1,14 +1,16 @@
 // content_scripts.js
-setTimeout(() => {
-    console.log("wait 2s..."); // 2초면 되겠지
-    initListener();
-}, 2000);
+initListener();
 
 function initListener() {
-    const title = document.querySelector('main > section > article > main > label').innerText
+    const title = document.querySelector(
+        "#educationList > div > li > div > ul > li:nth-child(" +
+        document.querySelectorAll("#educationList > div > li > div > ul > li.chk").length +
+        ")"
+    ).innerText
 
     document.querySelector('video').addEventListener('play', async function () {
         console.log('Video play event detected');
+        document.querySelector('video').volume = 0;
         const response = await chrome.runtime.sendMessage({action: 'sendNotification', status: 'started', title});
         console.log(response);
     });
@@ -17,5 +19,6 @@ function initListener() {
         console.log('Video ended event detected');
         const response = await chrome.runtime.sendMessage({action: 'sendNotification', status: 'ended', title});
         console.log(response)
+        document.querySelector('#popup5 > div.cmd > input').click();
     });
 }
